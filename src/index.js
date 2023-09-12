1688a6bbf7545b6e954cab64dfe2e64ccb86bb6b
Matt Meinzer <6733859+mmeinzer@users.noreply.github.com>
2023-09-12 15:32:43 -0700

    Init commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
+import { App as SendbirdApp } from "@sendbird/uikit-react";
+import "sendbird-uikit/dist/index.css";
 
+import "./index.css";
 export default class extends Component {
   render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+    return (
+      <SendbirdApp
+        appId={this.props.model?.sendbirdAppId}
+        accessToken={this.props.model?.accessToken}
+        userId={this.props.model?.userId}
+        nickname={this.props.model?.userNickname}
+        theme={"light"}
+        useReaction={true}
+        useMessageGrouping={true}
+      />
+    );
   }
 }
